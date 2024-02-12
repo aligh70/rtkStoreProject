0e906392a3fdeb86fb5ca1077d56d64bc2fd1478
Ali Ghaffari <aligh71@yahoo.com>
2024-02-12 13:22:10 +0330

    create cart slice

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -36,15 +36,13 @@ const getInitialQuery = (searchParams) => {
   return query;
 };
 
-const sumProducts = (products) => {
-  const itemsCounter = products.reduce(
-    (counter, product) => counter + product.quantity,
-    0
-  );
-  const total = products
+const sumPrice = (products) => {
+  return products
     .reduce((total, product) => total + product.price * product.quantity, 0)
     .toFixed(2);
-  return { itemsCounter, total };
+};
+const sumQuantity = (products) => {
+  return products.reduce((counter, product) => counter + product.quantity, 0);
 };
 
 const productQuantity = (state, id) => {
@@ -61,6 +59,7 @@ export {
   filterProducts,
   createQueryObject,
   getInitialQuery,
-  sumProducts,
   productQuantity,
+  sumPrice,
+  sumQuantity,
 };
